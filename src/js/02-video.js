67573a1caf896139018f2b73b0d88c2cd908ddf6
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2022-11-20 05:14:27 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,32 @@ import Throttle from "lodash.throttle";
 //let myStorage = window.localStorage;
 const keyLS = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
+const duration = player.getDuration();
 
 player.on('timeupdate', Throttle(saveCurrentTimeToLS, 1000));
 
 function saveCurrentTimeToLS(evt) {
     let currentTime = evt.seconds;
     localStorage.setItem(keyLS, currentTime);
-  //  console.log('Current time:', currentTime);
-  //  console.log(myStorage.getItem(keyLS));
+    
+  // console.log('Current time:', currentTime);
+  // console.log(localStorage.getItem(keyLS));
     };
+if (localStorage.getItem(keyLS)) {
+player.setCurrentTime(localStorage.getItem(keyLS));    
+}
 
-player.setCurrentTime(localStorage.getItem(keyLS));
+
+// якщо відео переглянуто повністю - видаляємо з Local Storage інформацію по ключу
+
+//function clearLocalStorage(duration) {
+//    if (Math.round(localStorage.getItem(keyLS)) === Math.round(duration)) {
+    //localStorage.removeItem(keyLS);
+    //localStorage.setItem(keyLS, 0);
+//    console.log("zeroed");
+//};
+
+//};
 
     //console.log('time to begin:', localStorage.getItem(keyLS));
